fdb8a1fa20fbc57403b74f8bcaa26c8e0bad5fe4
Renz Pulvira <calypsogaming098two@gmail.com>
2018-02-25 10:46:31 -0800

    Configured files

--- header.php
+++ header.php
@@ -7,20 +7,30 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md">
-  <div class="container">
-    <a href="#" class="navbar-brand"><?php echo get_bloginfo('name'); ?></a>
-    <?php
-
-    $header = array(
-      'container' => 'ul',
-      'menu_class' => 'navbar-nav ml-auto',
-      'container_class' => 'navbar-nav ml-auto',
-      'theme_location' => 'Primary'
-    );
-
-    wp_nav_menu($header);
-
-    ?>
+  <div class="container text-center">
+    <a href="#" class="navbar-brand"><h2><?php echo get_bloginfo('name'); ?></h2></a>
   </div>
-</nav>
+
+  <nav class="navbar navbar-expand-md">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php
+
+        $header = array(
+          'container' => 'ul',
+          'menu_class' => 'navbar-nav mx-auto',
+          'container_class' => 'navbar-nav mx-auto',
+          'theme_location' => 'Primary'
+        );
+
+        wp_nav_menu($header);
+
+        ?>
+
+      </div>
+    </div>
+  </nav>
---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+
+<div class="container">
+  <div class="cover column-layout">
+    <div class="column-layout-one">
+      <h2>This is a Title</h2>
+    </div>
+    <div class="column-layout-two">
+      <h2 class="display-4">This is another title</h2>
+    </div>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
